2092f0239d75225a06020da6dcf9b4d53897ed7f
HenryCROSS <henry583050973@gmail.com>
2024-05-03 00:21:52 -0400

    vga mode

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 static  HELLO: &[u8] = b"Hello World!";
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; // video memory address
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
